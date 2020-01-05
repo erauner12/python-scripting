0ccbcf522cb28711b10c53989d8288a90afa60c8
Evan Rauner <raunerevan@gmail.com>
2020-01-05 03:18:56 -0600

    started book tonight

---
+++ Python_Crash_Course/3-quiz_1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3.6
+print("")
+
+# Personal Message
+fname = "Evan"
+
+print("Hello " + fname)
+print("------")
+
+
+# Name Cases
+fname = "Evan"
+
+print("Hello " + fname.lower())
+print("Hello " + fname.upper())
+print("Hello " + fname.title())
+print("------")
+
+# Famous Quote
+
+print('Albert einstein once said,"A person who never made a mistake never tried anything new."')
+print("------")
+
+# Famous Quote 2
+
+famous_person = "Elvis Presley"
+
+print(famous_person + ' once said,"A person who never made a mistake never tried anything new."')
+print("------")
+
+# Stripping names
+
+username = " \nfsfsfe\n "
+username.lstrip()
+print(username)
+
+username.rstrip()
+print(username)
+
+username.strip()
+print(username)
+print("------")
+
+
+
+
+
---
+++ Python_Crash_Course/4-birthday-int-to-string-conversion.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.6
+print("")
+
+#demonstrating that you have to convert integers to strings to assign the value you are trying to create to a variable
+age = 26
+message = "Happy " + str(age) +  "th Birthday!"
+print(message)
+
+print("------")
+
+
+
+
