dad93a71878d525cb988d7a60594c8758115ce5b
Evan <raunerevan@gmail.com>
2020-01-06 18:33:04 -0600

    changing directory naming convention

---
+++ Linux/Python_Crash_Course/functions/python_1_greeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.6
+
+#/mnt/c/dev/python-scripting/Linux/Python_Crash_Course/functions
+
+import python_contain_functions
+
+python_contain_functions.greet_user('Evan')
+print("--------")
+
+
+python_contain_functions.display_message('Functions')
+print("--------")
+
+
+python_contain_functions.favorite_book("Phoenix Project")
+print("--------")
+
+
