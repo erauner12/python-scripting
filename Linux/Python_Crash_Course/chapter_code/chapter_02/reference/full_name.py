1f11ebcdab80a72ebc2cfcd097dfa16dbbb41573
Evan <raunerevan@gmail.com>
2020-01-07 22:13:37 -0600

    commit

--- Linux/Python_Crash_Course/chapter_code/chapter_02/reference/full_name.py
+++ Linux/Python_Crash_Course/chapter_code/chapter_02/reference/full_name.py
@@ -3,3 +3,9 @@ last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
 message = f"Hello, {full_name.title()}!"
 print(message)
+
+
+
+this_is_my_name = f"My name is {first_name} and my last name is {last_name}"
+
+print(this_is_my_name)
--- Linux/Python_Crash_Course/chapter_code/chapter_03/bicycles.py
+++ Linux/Python_Crash_Course/chapter_code/chapter_03/bicycles.py
@@ -2,3 +2,18 @@ bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 message = f"My first bicycle was a {bicycles[0].title()}."
 
 print(message)
+
+
+
+trucks = ['tundra', 'ford', 'chevy', 'ram']
+
+# using f string to insert multiple variables
+these_are_my_trucks = f"I own multiple trucks. I own a {trucks[2].title()}"
+
+print(these_are_my_trucks)
+
+# you can access each item in the list like this
+print(trucks[0])
+print(trucks[1])
+print(trucks[2])
+
